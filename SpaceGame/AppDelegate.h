354f28396e405bc9d5091785007aa8762848e45e
AndrewOwenWaldo <owenrhyfelwr@gmail.com>
2014-02-18 12:33:20 -0800

    Initial Commit

---
+++ SpaceGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpaceGame
+//
+//  Created by Andrew Rodgers on 2/18/14.
+//  Copyright (c) 2014 Andrew Rodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
